9fe5f0fb3b749ec692ba7b0be169531aab30d5b3
Piyush Yadav <piyushyadavcs@gmail.com>
2024-03-16 21:35:40 +0530

    update

--- script.js
+++ script.js
@@ -1,47 +1,41 @@
+/** @format */
+
 let arr = [
-    { id: 1, name: "john", age: "18", profession: "developer" },
-    { id: 2, name: "jack", age: "20", profession: "developer" },
-    { id: 3, name: "karen", age: "19", profession: "admin" },
+  { id: 1, name: "john", age: "18", profession: "developer" },
+  { id: 2, name: "jack", age: "20", profession: "developer" },
+  { id: 3, name: "karen", age: "19", profession: "admin" },
+];
+
+function PrintDeveloperbyMap() {
+  //Write your code here , just console.log
+  arr.map((emp)=> {if(emp.profession==="developer") console.log(emp)});
+}
+
+function PrintDeveloperbyForEach() {
+  //Write your code here , just console.log
+  arr.forEach((emp)=>{if(emp.profession==="developer") console.log(emp);});
+}
+
+function addData() {
+  //Write your code here, just console.log
+  let emp={id:4,name:"susan",age:"20",profession:"intern"};
+  arr.push(emp);
+  console.log(arr);
+}
+
+function removeAdmin() {
+  //Write your code here, just console.log
+  let newArr=arr.filter((emp)=>{return emp.profession!="admin"});
+  console.log(newArr);
+}
+
+function concatenateArray() {
+  //Write your code here, just console.log
+  let newArr = [
+    { id: 5, name: "emma", age: "22", profession: "Designer" },
+    { id: 6, name: "alex", age: "25", profession: "Assistant developer" },
+    { id: 7, name: "lucas", age: "23", profession: "Manager" },
   ];
-  
-  function PrintDeveloperbyMap() {
-    const developers = arr.filter(employee => employee.profession === "developer");
-    console.log(developers);
-  }
-  
-  function PrintDeveloperbyForEach() {
-    arr.forEach(employee => {
-      if (employee.profession === "developer") {
-        console.log(employee);
-      }
-    });
-  }
-  
-  function addData() {
-    const newEmployee = { id: 4, name: "susan", age: "20", profession: "intern" };
-    arr.push(newEmployee);
-    console.log(newEmployee);
-  }
-  
-  function removeAdmin() {
-    const filteredArr = arr.filter(employee => employee.profession !== "admin");
-    console.log(filteredArr);
-  }
-  
-  function concatenateArray() {
-    const newArray = [
-      { id: 5, name: "emma", age: "22", profession: "designer" },
-      { id: 6, name: "alex", age: "25", profession: "manager" },
-      { id: 7, name: "lucas", age: "23", profession: "developer" }
-    ];
-    const concatenatedArray = arr.concat(newArray);
-    console.log(concatenatedArray);
-  }
-  
-  // Testing the functions
-  PrintDeveloperbyMap();
-  PrintDeveloperbyForEach();
-  addData();
-  removeAdmin();
-  concatenateArray();
-  
+  let copy=arr.concat(newArr);
+  console.log(copy);
+}
